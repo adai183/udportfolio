fc1ebdddb030ca2168b40d3bf52722e177d7b61c
adai183 <andreas.daiminger@gmail.com>
2015-11-19 19:51:18 +0100

    Do a lot of stuff

---
+++ app/README.md
@@ -0,0 +1,29 @@
+## Website Performance Optimization portfolio project
+
+The challenge in this Udacity project was to optimize this online portfolio for speed! In particular, optimize the critical rendering path and make this page render as quickly as possible by applying the techniques I've picked up in the [Critical Rendering Path course](https://www.udacity.com/course/ud884).
+
+
+### My Optimizations
+
+
+* optimize app/css/style.css with gulp uncss
+* optimize images with gulp imagemin and other tools
+* inline CSS in index.html in production code with gulp critical
+
+* in app/view/js/main.js: 
+	1 generate 10 sliding pizzas and not 200.
+	2 make for loops more efficient by caching variables outside the for-loop
+	3 cache variable top in line 504
+	4 use getElementById/getElementByClass and not querySelector because of better performance
+
+* use <meta name=viewport content="width=device-width, initial-scale=1"> in head for index.html
+* async google anlytics script
+* media atribute to css links
+
+
+
+
+### My additional Resources 
+
+* [gulp tutorial on css tricks](https://css-tricks.com/gulp-for-beginners/)
+
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var uncss = require('gulp-uncss');
+var imagemin = require('gulp-imagemin');
+var critical = require('critical').stream;
+
+
+
+gulp.task('browserSync', function() {
+  browserSync({
+  	port: 8080,
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+
+gulp.task('watch', ['browserSync'], function (){
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+  gulp.watch('**/*.css', browserSync.reload);  
+});
+
+gulp.task('cleancss', function() {
+    gulp.src('**/*.css')
+        .pipe(uncss({
+            html: ['app/views/pizza.html',]
+        }))
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(imagemin({
+      // Setting interlaced to true
+      progressive: true
+    }))
+  .pipe(gulp.dest('dist/images'))
+});
+
+// Generate & Inline Critical-path CSS
+gulp.task('critical', function () {
+    return gulp.src('app/*.html')
+        .pipe(critical({base: 'app/', inline: true, css: ['app/css/style.css']}))
+        .pipe(gulp.dest('dist'));
+});
+
+
+
+   
+
+    
